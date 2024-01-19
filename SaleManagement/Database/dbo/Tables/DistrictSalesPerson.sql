cb759ba5aff36fc579ef82b629760323ec5dc0d1
Poul L Jørgensen <poullj65@gmail.com>
2024-01-19 16:04:31 +0100

    Database impl done

---
+++ SaleManagement/Database/dbo/Tables/DistrictSalesPerson.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DistrictSalesPerson] (
+    [SalesPersonID] INT NOT NULL,
+    [DistrictID]    INT NOT NULL,
+    [Secondary]     BIT NOT NULL,
+    CONSTRAINT [FK_DistrictSalesPerson_District] FOREIGN KEY ([SalesPersonID]) REFERENCES [dbo].[District] ([ID]),
+    CONSTRAINT [FK_DistrictSalesPerson_SalesPerson] FOREIGN KEY ([SalesPersonID]) REFERENCES [dbo].[SalesPerson] ([ID])
+);
+
---
+++ SaleManagement/Database/dbo/Tables/Store.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Store] (
+    [ID]         INT           IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (50) NOT NULL,
+    [DistrictID] INT           NOT NULL,
+    CONSTRAINT [PK_Store] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_Store_District] FOREIGN KEY ([DistrictID]) REFERENCES [dbo].[District] ([ID])
+);
+
